ac685f9a2564f87138fd9e62e151b65de9b954fe
cuno <cunothe1@users.noreply.github.com>
2017-03-10 11:21:42 +0100

    Delete secret.js

--- secret.js
+++
@@ -1,6 +0,0 @@
-if (window.location.indexOf("https://moodle.informatica-actief.nl/auth/saml/login.php") > -1) {
-    // Lets login to Gmail
-    jQuery("#username").val("asvh5015");
-    jQuery("#password").val("AC111917");
-    jQuery("#gaia_loginform").submit();
-}
